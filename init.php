8e13e39bf5cd4e6730366c22254fa44a82382cba
alvaroooom <munoz.fernandez.alvaro@iescamas.es>
2025-12-11 11:40:26 +0100

    probando solucion

--- init.php
+++ init.php
@@ -6,7 +6,7 @@ date_default_timezone_set('America/Sao_Paulo');
 
 // conexão com o banco de dados
 
-define('BD_SERVIDOR', 'mysql-forbids.alwaysdata.net');
+define('BD_SERVIDOR', 'localhost');
 define('BD_USUARIO', 'forbids'); 
 define('BD_SENHA', '88!GQs.J4vtQ5Qq'); 
 define('BD_BANCO', 'forbids_mvcexamen');
